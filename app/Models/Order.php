1824f40ae2a94537650c74a361e029dee73784d9
MD Jubair <mallickblogger@gmail.com>
2024-12-08 13:30:03 +0600

    feat: add Order model with relationships to Customer and OrderItem

---
+++ app/Models/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    //
+    protected $fillable = ['id', 'customer_id', 'total_price', 'status'];
+
+    /**
+     * An order belongs to a customer.
+     */
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+    /**
+     * An order can have many order items.
+     */
+    public function OrderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
