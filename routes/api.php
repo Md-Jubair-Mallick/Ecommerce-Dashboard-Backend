c6f083cc04e30fe31cc36a40979304a5295454f9
MD Jubair <mallickblogger@gmail.com>
2024-12-19 17:10:39 +0600

    fix(Auth): test and fix bugs in routes

--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
     /** ---------> Only admin can access <---------- */
     Route::middleware(['role:admin'])->group(function () {
 
-        /** ---------> Product Routes <---------- */
-        Route::controller(ProductController::class)->group(function () {
-            Route::delete('/products/{id}', 'destroy');
-            Route::post('/products', 'store');
-            Route::put('/products/{id}', 'update');
-        });
-
         /** ---------> Customer Routes <---------- */
         Route::controller(CustomerController::class)->group(function () {
             Route::post('/customers', 'store');
@@ -30,11 +23,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
             Route::delete('/customers/{id}', 'destroy');
         });
 
-        /** ---------> Review Routes <---------- */
-        Route::controller(ReviewController::class)->group(function () {
-            Route::put('reviews/{id}', 'update');
-            Route::delete('/reviews/{id}', 'destroy');
-        });
 
         /** ---------> Order Route <---------- */
         Route::put('/orders/{id}', [OrderController::class, 'update']);
@@ -45,6 +33,13 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     /** ---------> Only admin and editor can access <---------- */
     Route::middleware(['role:admin,editor'])->group(function () {
+        /** ---------> Product Routes <---------- */
+        Route::controller(ProductController::class)->group(function () {
+            Route::delete('/products/{id}', 'destroy');
+            Route::post('/products', 'store');
+            Route::put('/products/{id}', 'update');
+        });
+
         /** ---------> Order Routes <---------- */
         Route::controller(OrderController::class)->group(function () {
             Route::get('/orders/{id}', 'show');
@@ -57,14 +52,27 @@ Route::middleware(['auth:sanctum'])->group(function () {
             Route::get('/customers/{id}', 'show');
         });
 
+        /** ---------> Review Routes <---------- */
+        Route::controller(ReviewController::class)->group(function () {
+            Route::put('reviews/{id}', 'update');
+            Route::delete('/reviews/{id}', 'destroy');
+        });
     });
-    
+
     /** ---------> All roles can access <---------- */
     Route::middleware(['role:admin,editor,viewer'])->group(function () {
+        /** ---------> Analytics Routes <---------- */
+        Route::prefix('analytics')->group(function () {
+            Route::get('/sales', [AnalyticsController::class, 'getSalesData']);
+            Route::get('/revenue', [AnalyticsController::class, 'getRevenueData']);
+            Route::get('/customers', [AnalyticsController::class, 'getCustomerGrowthData']);
+        });
+        
         /** ---------> Product Routes <---------- */
         Route::get('/products', [ProductController::class, 'index']);
         Route::get('/products/{id}', [ProductController::class, 'show']);
         Route::get('/reviews', [ReviewController::class, 'index']);
+
     });
 
     /** ---------> Auth Routes <---------- */
@@ -76,10 +84,3 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
 /** ---------> Public Routes <---------- */
 Route::post('/auth/login', [AuthController::class, 'login']);
-
-/** ---------> Analytics Routes <---------- */
-Route::prefix('analytics')->group(function () {
-    Route::get('/sales', [AnalyticsController::class, 'getSalesData']);
-    Route::get('/revenue', [AnalyticsController::class, 'getRevenueData']);
-    Route::get('/customers', [AnalyticsController::class, 'getCustomerGrowthData']);
-});
